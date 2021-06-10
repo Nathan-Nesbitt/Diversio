75303a7bf5700310ade5f16566b8b6bc6fd5af81
Nathan Nesbitt <Nathan-Nesbitt@users.noreply.github.com>
2021-06-10 13:25:52 -0700

    Question 1 Solution

---
+++ Q1/Q1.py
@@ -0,0 +1,47 @@
+"""
+This program gets the current latitude/longitude of ISS
+The purpose of this task is to point out issues with the program below if any
+and improve it wherever applicable.
+
+Comments Nathan Nesbitt:
+
+This code seems pretty good. There were issues with the lettering, as they would
+always default to S and W. To fix it I moved them before the `if float() > 0` 
+lines which solves the problem. 
+
+I also removed the use of r.status_code on `if int() == 200:` and 
+`print("Request was unsuccessful: {}".format(st))` as it was already defined as
+the variable `st`. 
+
+"""
+
+import re
+import requests
+import time
+from time import strftime
+from time import localtime
+import json
+
+r = requests.get("http://api.open-notify.org/iss-now.json")
+d = json.loads(r.text)
+st = r.status_code
+
+if int(st) == 200:
+    ts = d["timestamp"]
+    time = strftime("%T", localtime(ts))
+
+    latitude = d["iss_position"]["latitude"]
+    latDir = "S"
+    if float(latitude) > 0:
+        latDir = "N"
+
+    longitude = d["iss_position"]["longitude"]
+    LonDir = "W"
+    if float(longitude) > 0:
+        LonDir = "E"
+
+    print("Current position of International Space Station at {}".format(time))
+    print("Latitude: {}° {}".format(latitude, latDir))
+    print("Longitude: {}° {}".format(longitude, LonDir))
+else:
+    print("Request was unsuccessful: {}".format(st))
