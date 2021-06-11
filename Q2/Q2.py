1633c3962631398115eb2a3bc35c1e45a5a24ed2
Nathan Nesbitt <Nathan-Nesbitt@users.noreply.github.com>
2021-06-11 14:14:16 -0700

    Updated function to return values instead of printing
    It is better practice to return values so they can be evaluated.

--- Q2/Q2.py
+++ Q2/Q2.py
@@ -55,6 +55,15 @@ from bs4 import BeautifulSoup
 
 
 def get_data(url):
+    """
+    Based on a url, prints a summary of the ping timings.
+
+    Takes: String URL of either the format
+            `https://stats.uptimerobot.com/api/getMonitor/<NAME>?m=<ID>` or
+            `https://status.uptimerobot.com/<ID>`
+
+    Returns: A dict with min, max, and median.
+    """
 
     # Splits the URL and gets the params
     split_url = url.split("/")
@@ -77,31 +86,29 @@ def get_data(url):
 
     sorted_values = sorted(d["monitor"]["responseTimes"], key=lambda k: k["value"])
 
-    # Prints out the result
-    print(
-        "Blog received highest ping on {} with value {}ms in last 90 days".format(
-            sorted_values[len(sorted_values) - 1]["datetime"],
-            sorted_values[len(sorted_values) - 1]["value"],
-        )
-    )
-    print(
-        "The min ping was {}ms on {}".format(
-            sorted_values[0]["value"], sorted_values[0]["datetime"]
-        )
-    )
+    results = {"min": {}, "max": {}, "median": None}
+
+    results["max"] = {
+        "datetime": sorted_values[len(sorted_values) - 1]["datetime"],
+        "value": sorted_values[len(sorted_values) - 1]["value"],
+    }
+
+    results["min"] = {
+        "datetime": sorted_values[0]["datetime"],
+        "value": sorted_values[0]["value"],
+    }
 
     # This is a faster way to compute median as the array is already sorted.
     index = (len(sorted_values) - 1) // 2
 
     if len(sorted_values) % 2:
-        print("The median was {}ms".format(sorted_values[index]["value"]))
+        results["median"] = sorted_values[index]["value"]
     else:
-        print(
-            "The median was {}ms".format(
-                (sorted_values[index]["value"] + sorted_values[index + 1]["value"])
-                / 2.0
-            )
-        )
+        results["median"] = (
+            sorted_values[index]["value"] + sorted_values[index + 1]["value"]
+        ) / 2.0
+
+    return results
 
 
 if __name__ == "__main__":
@@ -120,4 +127,17 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    get_data(args.url)
+    results = get_data(args.url)
+
+    # Prints out the results
+    print(
+        "Blog received highest ping on {} with value {}ms in last 90 days".format(
+            results["max"]["datetime"], results["max"]["value"]
+        )
+    )
+    print(
+        "The min ping was {}ms on {}".format(
+            results["min"]["value"], results["min"]["datetime"]
+        )
+    )
+    print("The median was {}ms".format(results["median"]))
